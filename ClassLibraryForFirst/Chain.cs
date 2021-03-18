272420f5676a9f3afd5df4f8be06356fa936ca2b
Agcitw <80104490+Agcitw@users.noreply.github.com>
2021-03-18 16:54:06 +0300

    Remade all logic of Validator

---
+++ ClassLibraryForFirst/Chain.cs
@@ -0,0 +1,48 @@
+﻿namespace ClassLibrary
+{
+	public abstract class ElementOfChain
+	{
+		public ElementOfChain Successor { get; set; }
+		public abstract void Handle(object data);
+	}
+
+	public class CheckOnNotNull : ElementOfChain
+	{
+		public override void Handle(object data)
+		{
+			if (!Rules.IsNotNull(data))
+				throw new BadCheckException();
+			Successor?.Handle(data);
+		}
+	}
+	
+	public class CheckOnPositiveNumber : ElementOfChain
+	{
+		public override void Handle(object data)
+		{
+			if (!Rules.IsPositiveNumber(data))
+				throw new BadCheckException();
+			Successor?.Handle(data);
+		}
+	}
+	
+	public class CheckOnZero : ElementOfChain
+	{
+		public override void Handle(object data)
+		{
+			if (!Rules.IsZero(data))
+				throw new BadCheckException();
+			Successor?.Handle(data);
+		}
+	}
+	
+	public class CheckOnStringWithoutUpperChars : ElementOfChain
+	{
+		public override void Handle(object data)
+		{
+			if (!Rules.IsStringWithoutUpperChars(data))
+				throw new BadCheckException();
+			Successor?.Handle(data);
+		}
+	}
+}
---
+++ ClassLibraryForFirst/Rules.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace ClassLibrary
+{
+	public static class Rules
+	{
+		public static bool IsNotNull(object data) =>
+			data != null;
+		public static bool IsPositiveNumber(object data) =>
+			data as int? >= 0;
+		public static bool IsStringWithoutUpperChars(object data) =>
+			(data as string ?? string.Empty).Count(char.IsUpper) == 0;
+		public static bool IsZero(object data) =>
+			data as int? == 0;
+	}
+}
