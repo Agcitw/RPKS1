113ab4685a534d09707db004a9de8d5ab8542d66
Agcitw <80104490+Agcitw@users.noreply.github.com>
2021-04-12 15:11:29 +0300

    Completed 3.1

--- Third/MainWindow.xaml.cs
+++ Third/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Third
 {
@@ -11,21 +13,35 @@ namespace Third
 		{
 			InitializeComponent();
 		}
-		
+
+		private int _imageInd = 1;
 		private readonly List<SolidColorBrush> _colorBrushesForeground = new List<SolidColorBrush>()
 		{
         	new SolidColorBrush(Colors.DarkBlue),
         	new SolidColorBrush(Colors.DarkGreen),
         	new SolidColorBrush(Colors.DarkRed),
-        	new SolidColorBrush(Colors.DarkOrchid),
+        	new SolidColorBrush(Colors.DarkOrchid)
 		};
-		
 		private readonly List<SolidColorBrush> _colorBrushesBackground = new List<SolidColorBrush>()
 		{
 			new SolidColorBrush(Colors.LightBlue),
 			new SolidColorBrush(Colors.LightGreen),
 			new SolidColorBrush(Colors.LightYellow),
-			new SolidColorBrush(Colors.LightCyan),
+			new SolidColorBrush(Colors.LightCyan)
+		};
+		private readonly List<SolidColorBrush> _colorBrushesScroll = new List<SolidColorBrush>()
+		{
+			new SolidColorBrush(Colors.Green),
+			new SolidColorBrush(Colors.Red),
+			new SolidColorBrush(Colors.Yellow),
+			new SolidColorBrush(Colors.Orange),
+			new SolidColorBrush(Colors.Pink),
+			new SolidColorBrush(Colors.RosyBrown),
+			new SolidColorBrush(Colors.Gold),
+			new SolidColorBrush(Colors.Orchid),
+			new SolidColorBrush(Colors.Lime),
+			new SolidColorBrush(Colors.GreenYellow),
+			new SolidColorBrush(Colors.CornflowerBlue),
 		};
 		
 		private void ButtonUp_OnClick(object sender, RoutedEventArgs e)
@@ -41,13 +57,28 @@ namespace Third
 
 		private void ChangeBackground_OnClick(object sender, RoutedEventArgs e)
 		{
-			Resources[@"ColorBrushBackground"] 
-				= _colorBrushesBackground[new Random().Next(0, _colorBrushesBackground.Count - 1)];
+			Resources[@"ColorBrushBackground"] =
+				_colorBrushesBackground[new Random().Next(0, _colorBrushesBackground.Count - 1)];
 		}
 		private void ChangeForeground_OnClick(object sender, RoutedEventArgs e)
 		{
-			Resources[@"ColorBrushForeground"] 
-				= _colorBrushesForeground[new Random().Next(0, _colorBrushesForeground.Count - 1)];
+			Resources[@"ColorBrushForeground"] =
+				_colorBrushesForeground[new Random().Next(0, _colorBrushesForeground.Count - 1)];
+		}
+
+		private void ButtonScroll_OnClick(object sender, RoutedEventArgs e)
+		{
+			Resources[@"ColorBrushScroll"] =
+				_colorBrushesScroll[new Random().Next(0, _colorBrushesScroll.Count - 1)];
+		}
+
+		private void ChangeImage_OnClick(object sender, RoutedEventArgs e)
+		{
+			var b1 = new BitmapImage(new Uri("Images/botimage (2).jpeg", UriKind.Relative));
+			var b2 = new BitmapImage(new Uri("Images/Apex-Legends-HD-Wallpapers-and-Background-Images-Wallpaper-Cart.png", UriKind.Relative));
+			var img = new Image {Source = _imageInd == 0 ? b1 : b2};
+			_imageInd = _imageInd == 0 ? 1 : 0;
+			Resources[@"MyImage"] = img;
 		}
 	}
 }
